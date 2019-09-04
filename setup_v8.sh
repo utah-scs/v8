07a3bfb8c1879fa2b4f0f447b18094330af0987f
Tian Zhang <tian@node.v8.sandstorm.emulab.net>
2019-09-04 03:06:50 -0600

    Add script to install dependancies.

---
+++ setup_v8.sh
@@ -0,0 +1,17 @@
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:`pwd`/depot_tools
+gclient config https://github.com/utah-scs/v8.git
+gclient sync
+sudo apt-get update
+./build/install-build-deps.sh
+alias gm=~/v8/tools/dev/gm.py
+git clone https://gn.googlesource.com/gn
+wget https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-linux.zip
+unzip ninja-linux.zip
+sudo cp ninja /usr/local/bin/
+sudo apt-get install clang
+cd gn
+python build/gen.py
+sudo ninja -C out
+cp out/gn /usr/local/bin/
+cd ~/v8
